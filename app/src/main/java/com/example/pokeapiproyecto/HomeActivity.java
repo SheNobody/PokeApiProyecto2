1ee05b6b2a2d6c5339c7615f548f66eb68ee9853
SheNobody <iriscc1498@gmail.com>
2019-11-08 12:27:14 -0600

    2.0

--- app/src/main/java/com/example/pokeapiproyecto/HomeActivity.java
+++ app/src/main/java/com/example/pokeapiproyecto/HomeActivity.java
@@ -42,23 +42,4 @@ public class HomeActivity extends AppCompatActivity {
         return null;
     }
 
-    private HomeActivity extends AsyncTask <>
-
-    {
-        @Override
-        protected String doInBackground (String...urls){
-
-        // Creamos el objeto URL desde el string que recibimos.
-        if (urls.length == 0) return "";
-        URL url = createUrl("https://pokeapi.co/api/v2/pokemon?offset=0&limit=150");
-        // Hacemos la petición. Ésta puede tirar una exepción.
-        String jsonResponse = "";
-        try {
-            jsonResponse = makeHttpRequest(url);
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "Problem making the HTTP request.", e);
-        }
-        return jsonResponse;
     }
-    }
-}
