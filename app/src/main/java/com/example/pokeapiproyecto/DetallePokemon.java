fdb116bba3246519791471e9e5c338e13c42fe9c
SheNobody <iriscc1498@gmail.com>
2019-11-11 18:17:32 -0600

    mas cambios

--- app/src/main/java/com/example/pokeapiproyecto/DetallePokemon.java
+++ app/src/main/java/com/example/pokeapiproyecto/DetallePokemon.java
@@ -3,12 +3,33 @@ package com.example.pokeapiproyecto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.pokeapiproyecto.data.PokemonShort;
 
 public class DetallePokemon extends AppCompatActivity {
+    PokemonShort pokemonShort;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_pokemon);
+        Parcelable extra = getIntent().getParcelableExtra(HomeActivity.EXTRA_DATA);
+        if (extra instanceof PokemonShort) {
+            pokemonShort = (PokemonShort) extra;
+        } else {
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        TextView textView = findViewById(R.id.pokemonInfo);
+        String info =
+                "ID: " + pokemonShort.getId() + "\n"
+                        + "NAME: " + pokemonShort.getName() + "\n"
+                        + "URL: " + pokemonShort.getUrl();
+        textView.setText(info);
     }
+
 }
--- app/src/main/java/com/example/pokeapiproyecto/adapter/PokemonAdapter.java
+++ app/src/main/java/com/example/pokeapiproyecto/adapter/PokemonAdapter.java
@@ -1,5 +1,48 @@
 package com.example.pokeapiproyecto.adapter;
 
-public class PokemonAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.pokeapiproyecto.R;
+import com.example.pokeapiproyecto.data.PokemonShort;
+
+import java.util.List;
+
+public class PokemonAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    List<PokemonShort> items;
+    PokemonItemListener listener;
+    public PokemonAdapter(List<PokemonShort> items, PokemonItemListener listener) {
+        this.items = items;
+        this.listener = listener;
+    }
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType
+    ) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_home, parent, false);
+        return new PokemonViewHolder(view, listener);
+    }
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof PokemonViewHolder) {
+            ((PokemonViewHolder) holder).decorateWith(items.get(position));
+        }
+    }
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public void setItems(List<PokemonShort> items) {
+        this.items = items;
+        notifyDataSetChanged();
+    }
+    public PokemonShort getItem(int position) {
+        if(items == null || items.size() <= position) return null;
+        return items.get(position);
+    }
 }
